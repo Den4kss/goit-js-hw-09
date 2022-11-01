73e4bf33a33d0651594aef3b883c88288675d54e
Den4kss <106112858+Den4kss@users.noreply.github.com>
2022-11-01 07:17:45 +0200

    ...

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -5,7 +5,7 @@ const bodyEl = document.querySelector('body');
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-
+let colorInterval;
 startRef.addEventListener('click', () => {
   startRef.disabled = true;
   return (colorInterval = setInterval(() => {
