3e9e32b451615458e75aa9d04808d52409c27afd
Den4kss <106112858+Den4kss@users.noreply.github.com>
2022-11-01 07:06:12 +0200

    ...

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,39 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+const ref = {
+  delay: document.querySelector('[name="delay"]'),
+  step: document.querySelector('[name="step"]'),
+  amount: document.querySelector('[name="amount"]'),
+  submit: document.querySelector('.form'),
+};
+ref.submit.addEventListener('submit', onStartPromise);
+
+function onStartPromise(evt) {
+  evt.preventDefault();
+  const delay = ref.delay.value;
+  const step = ref.step.value;
+  const amount = ref.amount.value;
+  let time = Number(delay);
+  for (let position = 1; position <= amount; position += 1) {
+    time += Number(step);
+    createPromise(position, time);
   }
+  console.log(Number(delay));
+}
+function createPromise(position, delay) {
+  const promice = new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, Number(delay));
+  });
+  promice
+    .then(({ position, delay }) => {
+      console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
 }
